e8414b149955998e92ce27532490fe509a534cf2
jyh2610 <jogg4177@naver.com>
2024-04-21 14:56:47 +0900

    ADD:슬라이드 컴포넌트

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,6 +1,7 @@
 //UI
 export { FullImage } from './ui/FullImage';
 export { Dropdown } from './ui/Dropdown';
+export { BasicSlider } from './ui/BasicSlider';
 
 //PATH
 export { PATH } from './constant/PATH';
--- src/shared/ui/BasicSlider.tsx
+++ src/shared/ui/BasicSlider.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import Slider from 'react-slick';
+import { FullImage } from './FullImage';
 
-function BasicSlider() {
-  return <div>BasicSlider</div>;
+interface Props {
+  imageUrl: string[];
 }
-
-export default BasicSlider;
+export function BasicSlider({ imageUrl }: Props) {
+  const settings = {
+    fade: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    waitForAnimate: false,
+    autoplay: true,
+    autoplaySpeed: 2000
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        {imageUrl.map((url, idx) => (
+          <div key={idx}>
+            <FullImage src={url} altContent="슬라이더 이미지" />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
